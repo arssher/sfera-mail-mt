87df5ce2cdb4e40c30f03564b24ad81d4d78f078
Arseny Sher <arsv.sher@gmail.com>
2016-10-06 12:35:22 +0300

    rewritten using shared_ptr

--- practice/1-allocator/allocator.cpp
+++ practice/1-allocator/allocator.cpp
@@ -6,41 +6,106 @@ std::ostream& operator<<(std::ostream &strm, const DataBlock &db) {
 }
 
 Allocator::Allocator(void *base, size_t size) {
-    buffer_start = reinterpret_cast<char*>(base);
+    buffer_start = static_cast<char*>(base);
     buffer_end = buffer_start + size;
-    std::cout << "constructing allocator, start address %p, end address %p\n" << buffer_start << buffer_end << "\n";
-    get_free_blocks();
+    std::cout << "constructing allocator, start address " <<
+                 (void*)buffer_start << ", end address " << (void*)buffer_end <<
+                 ", size " << buffer_end - buffer_start << "\n";
 }
 
-std::list<DataBlock> Allocator::get_free_blocks() {
+std::list<DataBlock> Allocator::get_free_blocks() const{
     std::list<DataBlock> free_blocks;
     char *free_block_start = buffer_start;
     char *free_block_end = nullptr;
-    std::list<DataBlock>::const_iterator busy_blocks_iterator = busy_blocks.begin();
+    std::list<std::shared_ptr<DataBlock>>::const_iterator busy_blocks_iterator = busy_blocks.begin();
     while (free_block_end != buffer_end) {
-        // get next block end
+        char *next_iteration_block_start = nullptr;
+        // get current free block end
         if (busy_blocks_iterator == busy_blocks.end()) {
-	    free_block_end = buffer_end;
-	} 
-	else {
-            DataBlock busy_block = *busy_blocks_iterator;
-	    free_block_end = busy_block.start;
-            busy_blocks_iterator++;
+            free_block_end = buffer_end;
+        } else {
+            DataBlock busy_block = **busy_blocks_iterator;
+            free_block_end = busy_block.start;
             // update block start for the next iteration
-            free_block_start = busy_block.end;
-	}
+            next_iteration_block_start = busy_block.end;
+            busy_blocks_iterator++;
+        }
         // now we ready to create block and push it to the result list
-        DataBlock free_block(free_block_start, free_block_end);
-        std::cout << "found free block " << free_block << "\n";
-        if (free_block.end - free_block.start > 0) {
-            std::cout << "pushed it\n";
-	    free_blocks.push_back(free_block);
-	}
+        if (free_block_end - free_block_start > 0) {
+            std::cout << "found free block " << DataBlock(free_block_start, free_block_end) << "\n";
+            free_blocks.push_back(DataBlock(free_block_start, free_block_end));
+        }
+        free_block_start = next_iteration_block_start;
     }
-    printf("exiting free_block\n");
     return free_blocks;
 }
 
+
 Pointer Allocator::alloc(size_t size) {
-    return Pointer();
+    std::list<DataBlock> free_blocks = get_free_blocks();
+    DataBlock block_to_allocate_from(nullptr, nullptr);
+    for (DataBlock free_db : free_blocks) {
+        if (free_db.size >= size &&  // this block is big enough for us
+            ((block_to_allocate_from.start == nullptr) || (block_to_allocate_from.size > free_db.size))) {
+                block_to_allocate_from = free_db;
+            }
+    }
+    if (block_to_allocate_from.start == nullptr)
+        throw AllocError(AllocErrorType::NoMemory, "Out of memory");
+    else {
+        DataBlock res_bloc = DataBlock(block_to_allocate_from.start, size);
+        return Pointer(add_busy_block(res_bloc));
+    }
+}
+
+std::shared_ptr<DataBlock> Allocator::add_busy_block(DataBlock db) {
+    std::cout << "Adding new busy block " << db << "\n";
+    auto it = busy_blocks.begin();
+    while (it != busy_blocks.end() && (*it)->start <= db.start)
+        it++;
+    return *busy_blocks.insert(it, std::make_shared<DataBlock>(db));
+}
+
+void Allocator::free(Pointer &p) {
+    for (auto busy_block_it = busy_blocks.begin(); busy_block_it != busy_blocks.end(); busy_block_it++) {
+        if (*busy_block_it == p.dbptr) {
+            std::cout << "Removing busy block " << **busy_block_it << "\n";
+            busy_blocks.erase(busy_block_it);
+            p.dbptr = nullptr;
+            return;
+        }
+    }
+    throw AllocError(AllocErrorType::InvalidFree, "Invalid free");
+}
+
+void Allocator::realloc(Pointer &p, size_t N) {
+    if (p.dbptr == nullptr) {
+        p = alloc(N);
+        return;
+    }
+    char *old_block_start = p.dbptr->start;
+    size_t old_size = p.dbptr->size;
+    free(p); // free the block so get_free_blocks will notice it
+    p = alloc(N);
+    char *new_block_start = p.dbptr->start;
+    // now copy data
+    for (size_t i = 0; i < old_size; i++) {
+        new_block_start[i] = old_block_start[i];
+    }
+}
+
+void Allocator::defrag() {
+    char *new_busy_block_start = buffer_start;
+    for (auto busy_block_ptr : busy_blocks) {
+        if (busy_block_ptr->start != new_busy_block_start) { // perhaps we don't need to move anything at all
+            size_t block_size = busy_block_ptr->size;
+            for (size_t i = 0; i < block_size; i++) {
+                new_busy_block_start[i] = busy_block_ptr->start[i];
+            }
+            std::cout << "moved block " << *busy_block_ptr;
+            *busy_block_ptr = DataBlock(new_busy_block_start, block_size);
+            std::cout << " to " << *busy_block_ptr << "\n";
+            new_busy_block_start += block_size;
+        }
+    }
 }
--- practice/1-allocator/allocator.h
+++ practice/1-allocator/allocator.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 #include <iostream>
+#include <memory>
 
 enum class AllocErrorType {
     InvalidFree,
@@ -36,41 +37,38 @@ public:
 
 class Pointer {
 public:
-    void *get() const { return 0; } 
+    Pointer() : dbptr(nullptr) {}
+    Pointer(std::shared_ptr<DataBlock> _dbptr) : dbptr(_dbptr) {}
+    void *get() const { return dbptr ? static_cast<void*>(dbptr->start) : nullptr; }
+    std::shared_ptr<DataBlock> dbptr;
 };
 
 class Allocator {
 public:
     Allocator(void *base, size_t size);
-    // Allocate N bytes and return pointer to them.
-    // 1) List all free blocks
-    // 2) Find the one with minimum size, but big enough for @size
-    // 3) Proclaim it. It means, if you store busy blocks, add the block
-    //   to the busy ones.
+    // Allocate N bytes and return pointer to them. It lists all free blocks, finds the one with minimum size,
+    // but big enough for @N and adds it to busy_blocks
     Pointer alloc(size_t N);
     
-    // Realloc memory block starting from @p. What we need here:
-    // remove old block from busy block list
-    // list of free blocks sorted by the size to choose the suitable one.
-    // + we need to add to this list @p block itself and merge it with the others.
-    // then alloc new size as usual and move the data there.
-    // we can also track should we move the data at all.
-    void realloc(Pointer &p, size_t N) {}
+    // Realloc memory block pointed by @p.
+    void realloc(Pointer &p, size_t N);
     
     // Free memory block starting from @p.
-    // If you store busy blocks, just find it and remove.
-    void free(Pointer &p) {}
+    // Just removed the block from busy_blocks and resets the pointer
+    void free(Pointer &p);
 
-    // iterate over busy blocks and move each to the left
-    void defrag() {}
+    // iterate over busy blocks and move each to the left side
+    void defrag();
     std::string dump() { return ""; }
 private:
     // calculate free blocks
-    std::list<DataBlock> get_free_blocks();
+    std::list<DataBlock> get_free_blocks() const;
+    // insert new busy block to correct place
+    std::shared_ptr<DataBlock> add_busy_block(DataBlock db);
 
     char *buffer_start;
     char *buffer_end;
     // ordered sequentially
-    std::list<DataBlock> busy_blocks;
+    std::list<std::shared_ptr<DataBlock>> busy_blocks;
 };
 
